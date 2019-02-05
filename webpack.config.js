802bd6563040079493a2a8bbea033588ee144c1c
Mike <mihail.xan@yandex.ua>
2019-02-05 13:54:07 +0200

    added minimizer webpack plugins for js and css

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const UnglifyjsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.join(__dirname, '/build'),
     filename: 'index_bundle.js'
-  },
-  devServer: {
-    historyApiFallback: true
   },
   module: {
     rules: [
@@ -19,17 +19,34 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
       },
       {
         test: /\.(ttf|eot|woff|woff2)$/,
-        use: 'file-loader'
+        use: [{
+          loader: 'file-loader',
+          options: {
+            outputPath: 'fonts/'
+          }
+        }]
       }
     ]
   },
   plugins: [
-    new HtmlWebpackPlugin({
+    new MiniCssExtractPlugin({
+      filename: 'index_bundle.css'
+    }),
+    new HtmlPlugin({
       template: './public/index.html'
     })
-  ]
+  ],
+  optimization: {
+    minimizer: [
+      new UnglifyjsPlugin(),
+      new OptimizeCssAssetsPlugin()
+    ]
+  },
+  devServer: {
+    historyApiFallback: true
+  }
 };
